99c66a4e03a6ba64ded71909cbc83efc45f477f3
Vinícius Santos Pereira <32989222+viniciussantos02@users.noreply.github.com>
2019-09-03 16:26:02 -0300

    Add files via upload
    Initial commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+//animação até o alvo
+$('nav a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - 89
+	}, 500);
+});
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+//animação scroll
+(function(){
+	var $target = $('.anime'),
+		animationClass = 'anime-start';
+		offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		})
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+		console.log('teste');
+	}, 50));
+
+}());
